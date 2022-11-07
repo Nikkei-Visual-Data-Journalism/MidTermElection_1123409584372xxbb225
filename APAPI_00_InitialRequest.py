83683b86b371de73e77501bbc6c63bd5a99e84f0
ku6otM <37606280+ku6otM@users.noreply.github.com>
2022-11-07 09:21:06 -0500

    1107

--- APAPI_00_InitialRequest.py
+++ APAPI_00_InitialRequest.py
@@ -38,7 +38,12 @@ for race in races:
     if 'seatNum' in race.keys():
         df['seatNum'] = race['seatNum']
     winners = pd.concat([winners, df])
-winners.to_csv(savefile, encoding='utf_8_sig', index=False)
+if len(winners) == 0:
+    pd.DataFrame(columns=['first', 'last', 'party', 'candidateID', 'polID', 'ballotOrder',
+       'polNum', 'voteCount', 'electWon', 'winner', 'winnerDateTime',
+       'statePostal', 'stateName', 'raceID']).to_csv(savefile, encoding='utf_8_sig', index=False)
+else:
+    winners.to_csv(savefile, encoding='utf_8_sig', index=False)
 
 
 
@@ -63,31 +68,37 @@ for race in races:
     if 'seatNum' in race.keys():
         df['seatNum'] = race['seatNum']
     winners = pd.concat([winners, df])
-winners.to_csv(savefile, encoding='utf_8_sig', index=False)
+if len(winners) == 0:
+    pd.DataFrame(columns=['first', 'last', 'party', 'incumbent', 'candidateID', 'polID',
+       'ballotOrder', 'polNum', 'voteCount', 'electWon', 'winner',
+       'winnerDateTime', 'statePostal', 'stateName', 'raceID', 'seatNum',
+       'abbrv', 'middle']).to_csv(savefile, encoding='utf_8_sig', index=False)
+else:
+    winners.to_csv(savefile, encoding='utf_8_sig', index=False)
     
-# Governor
-# Setting
-officeID = 'G'
-savefile = config['SaveFile']['governor'].replace('datadir', config['Path']['datadir'])
-# Request
-res = requests.get(
-    f'https://api.ap.org/v3/elections/{ELECTIONDATE}?apikey={APIKEY}&officeID={officeID}&resultstype={RESULTSTYPE}&winner=X&format=json')
-result = res.json()
-# save update info
-config['NextRequest']['governor'] = result['nextrequest'].replace('%', '%%')
+# # Governor
+# # Setting
+# officeID = 'G'
+# savefile = config['SaveFile']['governor'].replace('datadir', config['Path']['datadir'])
+# # Request
+# res = requests.get(
+#     f'https://api.ap.org/v3/elections/{ELECTIONDATE}?apikey={APIKEY}&officeID={officeID}&resultstype={RESULTSTYPE}&winner=X&format=json')
+# result = res.json()
+# # save update info
+# config['NextRequest']['governor'] = result['nextrequest'].replace('%', '%%')
 
-races = result['races']
-winners = pd.DataFrame()
-for race in races:
-    df = pd.DataFrame(race['reportingUnits'][0]['candidates'])
-    df['statePostal'] = race['reportingUnits'][0]['statePostal']
-    df['stateName'] = race['reportingUnits'][0]['stateName']
-    df['raceID'] = race['raceID']
-    if 'seatNum' in race.keys():
-        df['seatNum'] = race['seatNum']
-    winners = pd.concat([winners, df])
-winners.to_csv(savefile, encoding='utf_8_sig', index=False)
+# races = result['races']
+# winners = pd.DataFrame()
+# for race in races:
+#     df = pd.DataFrame(race['reportingUnits'][0]['candidates'])
+#     df['statePostal'] = race['reportingUnits'][0]['statePostal']
+#     df['stateName'] = race['reportingUnits'][0]['stateName']
+#     df['raceID'] = race['raceID']
+#     if 'seatNum' in race.keys():
+#         df['seatNum'] = race['seatNum']
+#     winners = pd.concat([winners, df])
+# winners.to_csv(savefile, encoding='utf_8_sig', index=False)
     
-# Save updated setting
-with open('APAPI.ini', 'w') as configfile:
-    config.write(configfile)
+# # Save updated setting
+# with open('APAPI.ini', 'w') as configfile:
+#     config.write(configfile)
