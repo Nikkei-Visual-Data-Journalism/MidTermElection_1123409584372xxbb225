2b90a000d1e251491632eafb3396c0a8a578b73b
ku6otM <37606280+ku6otM@users.noreply.github.com>
2022-11-08 16:11:28 -0500

    fadf

--- APAPI_01_MakeFlourish.py
+++ APAPI_01_MakeFlourish.py
@@ -7,8 +7,8 @@ bar_house = pd.read_csv('Flourish_templates/bar_house.csv', encoding='utf_8_sig'
 bar_senate = pd.read_csv('Flourish_templates/bar_senate.csv', encoding='utf_8_sig', dtype=object)
 
 # Import winners data
-house = pd.read_csv('APAPI/electionResultsWinners_house.csv', encoding='utf_8_sig')
-senate = pd.read_csv('APAPI/electionResultsWinners_senate.csv', encoding='utf_8_sig')
+house = pd.read_csv('APAPI/electionResultsWinners_house.csv', encoding='utf_8_sig', dtype=object)
+senate = pd.read_csv('APAPI/electionResultsWinners_senate.csv', encoding='utf_8_sig', dtype=object)
 
 # Import race ids
 raceID_house = pd.read_csv('raceID_house.csv', encoding='utf_8_sig', dtype=object)['raceID'].to_list()
@@ -19,10 +19,12 @@ senate = senate[senate.raceID.isin(raceID_senate)].copy()
 # Make map_house
 if len(house) > 0:
     _dict = {'Dem':'民主党', 'GOP':'共和党'}
+    _dict2 = map_house.set_index('districtID')['district'].to_dict()
     df = house.copy()
     df['districtID'] = df['statePostal'].astype(str) + df['seatNum'].astype(str).str.zfill(2)
     df['政党'] = df['party'].apply(lambda x: _dict[x] if x in _dict.keys() else 'その他')
     map_house = pd.merge(map_house[['geometry', 'districtID']], df[['districtID', '政党']], on='districtID', how='left').fillna('残り')
+    map_house['district'] = map_house['districtID'].apply(lambda x: _dict2[x])
 map_house.to_csv('Flourish/map_house.csv', encoding='utf_8_sig', index=False)
 
 # Make map_senate
@@ -30,7 +32,7 @@ if len(senate) > 0:
     for i, row in map_senate[map_senate['議席']=='改選'].iterrows():
         t_df = senate[senate.raceID==row.raceID].copy()
         if len(t_df) == 1:
-            map_senate.loc[i, '結果'] = t_df.iloc[0].party.apply(lambda x: _dict[x] if x in _dict.keys() else 'その他')
+            map_senate.loc[i, '結果'] = _dict[t_df.iloc[0].party] if t_df.iloc[0].party in _dict.keys() else 'その他'
 map_senate.to_csv('Flourish/map_senate.csv', encoding='utf_8_sig', index=False)
 
 # Make bar_house
