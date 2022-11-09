d640db2038d2a6ee1149b6a21d09dec5b03eda86
ku6otM <37606280+ku6otM@users.noreply.github.com>
2022-11-09 02:40:47 -0500

    Update APAPI_01_MakeFlourish.py

--- APAPI_01_MakeFlourish.py
+++ APAPI_01_MakeFlourish.py
@@ -58,8 +58,8 @@ if len(senate) > 0:
     for c in ['民主党', 'その他', '共和党']:
         if c not in result.keys():
             result[c] = 0
+    result['共和党'] += 1
     for c in ['民主党', 'その他', '共和党']:
         bar_senate.iloc[0][c] = result[c]
-    bar_senate.iloc[0]['共和党'] += 1
     bar_senate.iloc[0]['残り'] = 35 - sum(result.values())
 bar_senate.to_csv('Flourish/bar_senate.csv', encoding='utf_8_sig', index=False)
